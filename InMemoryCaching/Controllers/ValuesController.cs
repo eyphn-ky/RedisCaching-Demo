e5d315c38a34b6fc0d23751333ca323223d6a84e
EYUPHAN\eyuph <eyuphan.kaya34@gmail.com>
2023-08-10 23:50:22 +0300

    Add project files.

---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+              _memoryCache = memoryCache;
+        }
+    }
+}
