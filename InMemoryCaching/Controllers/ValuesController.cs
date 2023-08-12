5134029d88bc2f27f33f52c0245bd40405a929dc
EYUPHAN\eyuph <eyuphan.kaya34@gmail.com>
2023-08-13 00:26:48 +0300

    simple caching

--- InMemoryCaching/Controllers/ValuesController.cs
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -13,5 +13,37 @@ namespace InMemoryCaching.Controllers
         {
               _memoryCache = memoryCache;
         }
+
+        [HttpGet("set/{value}")]
+        public void Set(string value)
+        {
+            _memoryCache.Set("name",value);
+        }
+
+        [HttpGet("get")]
+        public string Get()
+        {
+            if(_memoryCache.TryGetValue<string>("name", out string name)) //varsa al. out keywprd ile içerdeli verinin referansını aldık.
+            {
+                return name.Substring(3);
+            }
+            return "";
+            //return _memoryCache.Get<string>("name");
+        }
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now,options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+
+        }
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
     }
 }
