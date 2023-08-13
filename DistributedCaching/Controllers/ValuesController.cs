e0a2d43aa4fc7274f45d2994d9948ab27f4a99de
EYUPHAN\eyuph <eyuphan.kaya34@gmail.com>
2023-08-13 22:31:51 +0300

    distributed caching demo

---
+++ DistributedCaching/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace DistributedCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name,string surname)
+        {
+            await _distributedCache.SetStringAsync("name",name,options: new()
+            {
+                AbsoluteExpiration=DateTime.Now.AddSeconds(30),
+                SlidingExpiration=TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("surname",Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+            var surnameBinary= await _distributedCache.GetAsync("name");
+            var surname = Encoding.UTF8.GetString(surnameBinary);
+            return Ok(
+                new
+                {
+                    name,
+                    surname
+                });
+        }
+    }
+}
--- DistributedCaching/Program.cs
+++ DistributedCaching/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddStackExchangeRedisCache(options => options.Configuration= "localhost:1453");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
